88ed6b0b216000120834612c6942698912500c93
Abhiram Tamvada <65614195+Abh1T@users.noreply.github.com>
2021-06-21 16:44:18 -0400

    Create Block.java

---
+++ SideScroller/Block.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Block {
+
+	int blockX, blockY, width, height;
+	String type;
+
+	public Block(int blockX, int blockY, int width, int height, String type) {
+		this.blockX = blockX;
+		this.blockY = blockY;
+		this.width = width;
+		this.height = height;
+		this.type = type;
+	}
+	public int getX() {
+		return blockX;
+	}
+	public int getY() {
+		return blockY;
+	}
+	public String getType() {
+		return type;
+	}
+	public boolean isTopBlock() {
+		return type.equals("B");
+	}
+	public void updateX(int num) {
+		blockX += num;
+		if (blockX == -50)
+			blockX += 1000;
+	}
+	public Rectangle2D getCollisionBox() {
+		return new Rectangle2D.Double(getX(), getY(), width, height);
+	}
+}
